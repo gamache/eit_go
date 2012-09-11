4ed6afadeeb3be8b4297c49674e7ebf4cdeeac6f
pete gamache <pete@where.com>
2012-09-11 10:41:23 -0400

    added Game model

---
+++ app/models/game.go
@@ -0,0 +1,36 @@
+package "models"
+
+type Game struct {
+  TourneyId string `json:"tourney_id"`
+  UserId string `json:"user_id"`
+  GameId string `json:"game_id"`
+
+  /// xlogfile fields follow
+  Version string
+  Points int
+  Deathdnum int
+  Deathlev int
+  Maxlvl int
+  Hp int
+  Maxhp int
+  Deaths int
+  Deathdate int // yyyymmdd
+  Birthdate int // yyyymmdd
+  Uid int
+  Role string
+  Race string
+  Gender string
+  Align string
+  Name string
+  Death string
+  Conduct int // bitfield
+  Turns int
+  Achieve int // bitfield
+  Realtime int // seconds
+  Starttime int // seconds since epoch
+  Endtime int // seconds since epoch
+  Gender0 string
+  Align0 string
+}
+
+
